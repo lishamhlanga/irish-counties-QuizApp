8dae335b8bea9a27e0789203c8188cd828681c53
lishamhlanga <lishamhlanga@gmail.com>
2022-03-09 03:31:36 +0000

    Add the declare  UI elements javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,10 @@
 
-
-const restartBtn = document.getElementById("restart");
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-const submitBtn = document.getElementById("submit");
-const trueBtn = document.getElementById("True");
-const falseBtn = document.getElementById("False");
-const playerScore = document.getElementById("player-score");
-const totalScore = document.getElementById("total-score");
-const question = document.getElementById("question");
-
-
-function beginQuiz() {
-}
-
-function restart() {
-}
-
-function next() {
-}
-
-function prev() {
-
-}
-
-function submit() {
-
-}
+//declare the UI elememts
+const ul = document.getElementById('ul');
+const nextButton = document.getElementById('btnNext');
+const quizbox = document.getElementById('questionArea');
+const opt1 = document.getElementById('opt1');
+const opt2 = document.getElementById('opt2');
+const opt3 = document.getElementById('opt3');
+const opt4 = document.getElementById('opt4');
 
